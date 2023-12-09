45a772860ae71e9433c108a376bd1bf851804579
Franklin Yiu <67379586+llama887@users.noreply.github.com>
2023-12-09 14:22:36 -0500

    probably working login and register

--- authentication/auth_system/urls.py
+++ authentication/auth_system/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import HomePage, Register, Login, logoutuser
+from .views import (
+    Customer_HomePage,
+    Staff_HomePage,
+    Customer_Register,
+    Login,
+    logoutuser,
+)
 
 urlpatterns = [
-    path("home/", HomePage, name="home-page"),
-    path("register/", Register, name="register-page"),
+    path("customer/", Customer_HomePage, name="customer-homepage"),
+    path("staff/", Staff_HomePage, name="staff-homepage"),
+    path("customer/register/", Customer_Register, name="customer-register"),
+    path("staff/register/", Customer_Register, name="staff-register")
     path("login/", Login, name="login-page"),
     path("logout/", logoutuser, name="logout"),
 ]
--- authentication/auth_system/views.py
+++ authentication/auth_system/views.py
@@ -1,11 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-
-
-# Create your views here.
+from .models import CustomerProfile, StaffProfile
 
 
 @login_required
@@ -13,22 +10,66 @@ def HomePage(request):
     return render(request, "auth_system/index.html", {})
 
 
-def Register(request):
+def Customer_Register(request):
     if request.method == "POST":
+        # Extract user information from the form
         fname = request.POST.get("fname")
-        lname = request.POST.get("sname")
-        name = request.POST.get("uname")
-        email = request.POST.get("email")
+        lname = request.POST.get("lname")
+        email = request.POST.get("uname")
         password = request.POST.get("pass")
+        building_number = request.POST.get("building_number")
+        street = request.POST.get("street")
+        city = request.POST.get("city")
+        apartment_number = request.POST.get("apartment_number")
+        state = request.POST.get("state")
+        zipcode = request.POST.get("zipcode")
+        passport_number = request.POST.get("passport_number")
+        passport_expiration = request.POST.get("passport_expiration")
+        passport_country = request.POST.get("passport_country")
+        date_of_birth = request.POST.get("date_of_birth")
 
-        new_user = User.objects.create_user(name, email, password)
+        # Create a new user
+
+        new_user = User.objects.create_user(
+            username=email, email=email, password=password
+        )
         new_user.first_name = fname
         new_user.last_name = lname
-
         new_user.save()
+
+        # Create a new customer profile associated with the user
+        CustomerProfile.objects.create(
+            user=new_user,
+            building_number=building_number,
+            street=street,
+            city=city,
+            apartment_number=apartment_number,
+            state=state,
+            zipcode=zipcode,
+            passport_number=passport_number,
+            passport_expiration=passport_expiration,
+            passport_country=passport_country,
+            date_of_birth=date_of_birth,
+        )
+
         return redirect("login-page")
 
     return render(request, "auth_system/register.html", {})
+
+
+# def Login(request):
+#     if request.method == "POST":
+#         name = request.POST.get("uname")
+#         password = request.POST.get("pass")
+
+#         user = authenticate(request, username=name, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect("home-page")
+#         else:
+#             return HttpResponse("Error, user does not exist")
+
+#     return render(request, "auth_system/login.html", {})
 
 
 def Login(request):
@@ -39,7 +80,12 @@ def Login(request):
         user = authenticate(request, username=name, password=password)
         if user is not None:
             login(request, user)
-            return redirect("home-page")
+            if hasattr(user, "customerprofile"):
+                return redirect("customer-homepage")
+            elif hasattr(user, "staffprofile"):
+                return redirect("staff-homepage")
+            else:
+                return HttpResponse("Error, user does not have a valid profile")
         else:
             return HttpResponse("Error, user does not exist")
 
